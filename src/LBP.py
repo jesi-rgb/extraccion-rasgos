4ff00a6656d9b98efd11c45b62b2ebde46f4a6ef
Jesús Enrique Rascón <blograso@gmail.com>
2021-02-09 12:15:59 +0100

    structure of lbp

--- src/LBP.py
+++ src/LBP.py
@@ -2,3 +2,13 @@
 author: Jesús Enrique Cartas Rascón
 repo: https://github.com/jesi-rgb/extraccion-rasgos
 '''
+
+
+class LBPDescriptor():
+    def __init__(self, parámetros ):
+    # Declaración e inicialización de variables de instancia
+
+    
+    def compute(self, img):
+    # Calcula LBP sobre 'img' y devuelve el descriptor
+        return descriptors
